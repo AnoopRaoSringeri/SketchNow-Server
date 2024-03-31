434c15c5625682418a6f0a07e3e79d6b9f962b65
Anoop Rao G N <anoopginigini@gmail.com>
2024-04-01 00:57:24 +0530

    Initial server setup

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const app: Express = express();
+
+app.use(cors());
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+  console.log("Request received");
+  res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
