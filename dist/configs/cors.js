94b8a94ddc9777fd684107485f997674c99798d5
Anoop Rao G N <anoopginigini@gmail.com>
2024-06-12 22:17:18 +0530

    cors

--- dist/configs/cors.js
+++ dist/configs/cors.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const corsOptions = {
-    // origin: ["https://sketchnow-client.onrender.com", "https://localhost:3000"],
-    // credentials: true,
-    allowedHeaders: "*",
-    origin: "*",
+    origin: ["https://sketchnow-client.onrender.com", "https://localhost:3000"],
+    credentials: true,
+    // allowedHeaders: "*",
+    // origin: "*",
 };
 exports.default = corsOptions;
