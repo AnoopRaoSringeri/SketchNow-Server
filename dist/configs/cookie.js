c84164a4e76e42b553487faaa02caf2cf99a3da9
Anoop Rao G N <anoopginigini@gmail.com>
2024-07-19 21:08:42 +0530

    build fix

--- dist/configs/cookie.js
+++ dist/configs/cookie.js
@@ -4,5 +4,6 @@ const cookieConfig = {
     httpOnly: true,
     expires: new Date(Date.now() + 60 * 1000 * 60 * 100000),
     sameSite: "none",
+    secure: true,
 };
 exports.default = cookieConfig;
--- src/configs/cookie.ts
+++ src/configs/cookie.ts
@@ -4,6 +4,7 @@ const cookieConfig: CookieOptions = {
   httpOnly: true,
   expires: new Date(Date.now() + 60 * 1000 * 60 * 100000),
   sameSite: "none",
+  secure: true,
 };
 
 export default cookieConfig;
