b4342768338b9e2367fced314311e8443530d997
Anoop Rao G N <anoopginigini@gmail.com>
2024-07-19 21:12:46 +0530

    build fix

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 /* eslint-disable no-underscore-dangle */
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
@@ -15,7 +13,7 @@ const hbs_1 = __importDefault(require("hbs"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const path_1 = __importDefault(require("path"));
 const configs_1 = require("./configs");
-// const login_1 = __importDefault(require("./middlewares/login"));
+const login_1 = __importDefault(require("./middlewares/login"));
 const v1_1 = require("./routes/v1");
 dotenv_1.default.config();
 const port = process.env.PORT;
@@ -29,22 +27,21 @@ app.set("view engine", "html");
 app.set("views", path_1.default.join(__dirname, "views"));
 app.engine("html", hbs_1.default.__express);
 app.use("/", v1_1.authRouter);
-// app.use(login_1.default);
+app.use(login_1.default);
 app.use("/", v1_1.sketchRouter);
 const start = async () => {
-  try {
-    await mongoose_1.default.connect(
-      `mongodb+srv://SketchNow:${process.env.MONGO_PASSWORD}@phoenix.jhaaso5.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`,
-    );
-    // https.createServer(serverOptions, app).listen(port, () => {
-    //   console.log(`Server started on port ${port}`);
-    // });
-    app.listen(port, () => {
-      console.log(`App is Listening on PORT ${port}`);
-    });
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+    try {
+        await mongoose_1.default.connect(`mongodb+srv://SketchNow:${process.env.MONGO_PASSWORD}@phoenix.jhaaso5.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`);
+        // https.createServer(serverOptions, app).listen(port, () => {
+        //   console.log(`Server started on port ${port}`);
+        // });
+        app.listen(port, () => {
+            console.log(`App is Listening on PORT ${port}`);
+        });
+    }
+    catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
 };
 start();
