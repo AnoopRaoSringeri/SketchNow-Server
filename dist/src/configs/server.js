b091ae5bbb3289364cad864536459cd4e1b4ee2f
Anoop Rao G N <anoopginigini@gmail.com>
2025-01-22 22:15:22 +0530

    build fix

--- dist/src/configs/server.js
+++ dist/src/configs/server.js
@@ -1,11 +1,7 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
 const serverOptions = {
-    key: fs_1.default.readFileSync("../../cert/key.pem"),
-    cert: fs_1.default.readFileSync("../../cert/cert.pem"),
+// key: fs.readFileSync("../../cert/key.pem"),
+// cert: fs.readFileSync("../../cert/cert.pem"),
 };
 exports.default = serverOptions;
