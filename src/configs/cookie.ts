3e0fbd15134073e7261d11894333bcdea4771328
Anoop Rao G N <anoopginigini@gmail.com>
2025-09-07 22:13:40 +0530

    build update

--- src/configs/cookie.ts
+++ src/configs/cookie.ts
@@ -1,10 +1,13 @@
 import { CookieOptions } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const cookieConfig: CookieOptions = {
   httpOnly: true,
   expires: new Date(Date.now() + 60 * 1000 * 60 * 100000),
-  sameSite: "none",
-  secure: true,
+  sameSite: process.env.SECURE === "True" ? "none" : "lax",
+  secure: process.env.SECURE ? process.env.SECURE === "True" : false
 };
 
 export default cookieConfig;
